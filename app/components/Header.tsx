f0f9db835c499d5088c3a0a3d364e2a52062cc2b
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-14 11:58:14 +0300

    update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { getCookie } from '../../util/cookies';
 import { parseJson } from '../../util/json';
+import BurgerMenu from './BurgerMenu';
 
 export default async function Header() {
   const cartCookie = await getCookie('cart');
@@ -14,25 +15,28 @@ export default async function Header() {
   );
 
   return (
-    <header>
-      <Link href="/">
-        <h2 className="lowercase text-[#0e372e] font-grotesk text-[1.6rem] bg-[#3acda8] h-full tracking-[0.3rem] font-black px-5 leading-[3.2rem]">
-          Vector
-        </h2>
-      </Link>
-      <nav>
-        <div>
-          <Link href="/viruses">Viruses</Link>
-        </div>
-        <Link href="/about">About</Link>
-        <Link href="/cart" data-test-id="cart-link">
-          Cart:{' '}
-          <span data-test-id="cart-count">
-            {`[${totalCartItems ? totalCartItems : 'empty'}]`}
-          </span>
+    <>
+      <header className="normalMenu">
+        <Link href="/">
+          <h2 className="lowercase text-[#0e372e] font-grotesk text-[1.6rem] bg-[#3acda8] h-full tracking-[0.3rem] font-black px-5 leading-[3.2rem]">
+            Vector
+          </h2>
         </Link>
-        <Link href="/checkout">Checkout</Link>
-      </nav>
-    </header>
+        <nav>
+          <div>
+            <Link href="/viruses">Viruses</Link>
+          </div>
+          <Link href="/about">About</Link>
+          <Link href="/cart" data-test-id="cart-link">
+            Cart:{' '}
+            <span data-test-id="cart-count">
+              {`[${totalCartItems ? totalCartItems : 'empty'}]`}
+            </span>
+          </Link>
+          <Link href="/checkout">Checkout</Link>
+        </nav>
+      </header>
+      <BurgerMenu />
+    </>
   );
 }
