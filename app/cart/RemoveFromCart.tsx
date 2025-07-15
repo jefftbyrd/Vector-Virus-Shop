bd7ce27431079673bc4236902a8789ca47e6fd98
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-15 15:02:02 +0300

    update remove from cart

--- app/cart/RemoveFromCart.tsx
+++ app/cart/RemoveFromCart.tsx
@@ -9,11 +9,11 @@ export default function RemoveFromCart(props: Props) {
   return (
     <form className="h-full">
       <button
-        className="h-full uppercase bg-light-blue text-dark-green text-sm font-bold w-auto px-5 border-0"
+        className="h-full flex flex-row gap-2 items-center uppercase border-1 border-light-blue/70 text-light-blue text-xs sm:text-sm font-bold p-2"
         formAction={() => removeItem(props.cartItemId)}
         data-test-id="cart-product-remove-<product id>"
       >
-        Remove
+        <span>x</span> <span className="uppercase">Remove</span>
       </button>
     </form>
   );
