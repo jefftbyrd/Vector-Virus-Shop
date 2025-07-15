85cb4a28a7eafd9bef916654ff648387e92a3826
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-15 16:48:33 +0300

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer grid grid-cols-2 gap-5 px-5 pt-5 pb-10 sm:grid-cols-4 lg:px-10 xl:px-40 sm:pt-10 md:pb-20 bg-black/30 border-t-1 border-light-green mt-auto">
       <div>
         <h3>Navigation</h3>
         <ul>
@@ -36,9 +36,11 @@ export default function Footer() {
       <div>
         <h3>Sign up for our Newsletter</h3>
         <form className="newsletter">
-          <label>
+          <label className="flex flex-col gap-2">
             <input type="email" placeholder="email address" required />
-            <button className="buttonBlue">Subscribe</button>
+            <button className="px-3 py-2 bg-light-blue text-dark-green uppercase font-bold font-ddin rounded-lg text-sm sm:text-lg w-3/4 lg:w-1/2">
+              Subscribe
+            </button>
           </label>
         </form>
       </div>
