056274880116e5c97e279f8b9080a3b5c41ce18f
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-15 16:48:22 +0300

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,20 @@ import News from './components/News';
 export default function Home() {
   return (
     <>
-      <div className="mb-10 lg:mt-12 xl:mb-55 hidden sm:block">
+      <div className="mb-10 lg:mt-12 sm:mb-14 md:mb-21  lg:mb-27 xl:mb-55 hidden sm:block">
         <h1 className="font-grotesk font-black lg:justify-self-center text-center lowercase text-[#3acda8] text-[30vw] lg:text-[26vw] leading-none lg:leading-[12vw] -z-1">
           Vector
         </h1>
-        <h2 className="lg:mt-[9vh] text-center lg:text-right relative lg:-translate-x-[18.5vw] text-xl lg:text-[3.7vw] text-white -z-10 uppercase font-thin font-grotesk">
+        <div className="sm:mt-[0vh] lg:mt-[6vh] xl:mt-[9vh] text-right -translate-x-[18.5vw] text-[3.7vw] text-white -z-10 uppercase font-thin font-grotesk">
           The virus shop
-        </h2>
-        <div className="absolute z-10 lg:top-[16vh] lg:left-[23.6vw] w-2/5 aspect-[1648/1416] justify-self-center -translate-y-27">
+        </div>
+        <div className="absolute z-10 sm:top-[18vh] sm:top-[20vh] md:top-[23vh] xl:top-[21vh] 2xl:top-[24vh] sm:left-[21.5vw] md:left-[21.5vw] lg:left-[23.6vw] w-2/5 aspect-[1648/1416] justify-self-center -translate-y-27">
           <Image
             src="/viruses/adenovirus-home.webp"
             alt="Vector, the virus shop"
             width={500} // Set desired width
             height={300} // Set actual height (for aspect ratio)
-            className="lg:w-[27vw] h-auto"
+            className="sm:w-[27vw] h-auto"
           />
         </div>
       </div>
