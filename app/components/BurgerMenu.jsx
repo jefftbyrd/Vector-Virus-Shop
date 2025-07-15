18f1fb5b223fe2080eafa7b002c72e39df03e78d
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-15 10:50:08 +0300

    update burger menu

--- app/components/BurgerMenu.jsx
+++ app/components/BurgerMenu.jsx
@@ -1,5 +1,6 @@
 'use client';
-// import './BurgerMenu.css'; // Optional: for custom styles
+import Image from 'next/image';
+import Link from 'next/link';
 import React, { useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 
@@ -16,25 +17,47 @@ const BurgerMenu = () => {
 
   return (
     <div className="burgerMenu">
-      <Menu
-        // right
-        pushRotate
-        pageWrapId={'page-wrap'}
-        outerContainerId={'outer-container'}
-        isOpen={isOpen}
-        onStateChange={handleStateChange}
-      >
-        <a onClick={closeMenu} className="menu-item" href="/">
-          Home
-        </a>
-        <a onClick={closeMenu} className="menu-item" href="/about">
-          About
-        </a>
-        <a onClick={closeMenu} className="menu-item" href="/contact">
-          Contact
-        </a>
-        {/* Add more menu items as needed */}
-      </Menu>
+      <div className="">
+        <Menu
+          pageWrapId="pageWrap"
+          outerContainerId="outerContainer"
+          isOpen={isOpen}
+          onStateChange={handleStateChange}
+          width="50%"
+        >
+          <Link onClick={closeMenu} className="menu-item" href="/">
+            Home
+          </Link>
+          <Link onClick={closeMenu} className="menu-item" href="/viruses">
+            Viruses
+          </Link>
+          <Link onClick={closeMenu} className="menu-item" href="/about">
+            About
+          </Link>
+          <Link onClick={closeMenu} className="menu-item" href="/cart">
+            Cart
+          </Link>
+        </Menu>
+      </div>
+      <div className="pb-2">
+        <Link href="/">
+          <h2>Vector</h2>
+        </Link>
+        <Link href="/">
+          <h3>The Virus Shop</h3>
+        </Link>
+      </div>
+      <div className="w-auto absolute right-2 -translate-y-2 top-0">
+        <Link href="/">
+          <Image
+            src="/viruses/adenovirus-home.webp"
+            alt="Vector, the virus shop"
+            width={80} // Set desired width
+            height={30} // Set actual height (for aspect ratio)
+            className="h-auto drop-shadow-md/30"
+          />
+        </Link>
+      </div>
     </div>
   );
 };
