a5930ff5ea943a477218bf464433e44da5c7accf
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-17 11:10:12 +0300

    update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -16,28 +16,36 @@ export default async function Header() {
 
   return (
     <header className="">
-      <div className="normalMenu top-0 sm:sticky top-0 z-100 hidden sm:flex items-center h-16">
-        <div className="flex flex-row h-full bg-light-green items-center logoContainer text-dark-green">
-          <Link href="/">
-            <h2 className="lowercase font-grotesk text-[1.7rem] leading-none tracking-[0.3rem] font-black h-full px-4 -translate-y-1">
-              Vector
-            </h2>
-          </Link>
-        </div>
+      <div className="bg-dark-green h-16 hidden sm:block">
+        <Link href="/">
+          <h3 className="absolute top-2 left-5 lowercase font-grotesk text-4xl tracking-widest font-black text-light-green border-b-2 pb-1 border-white hover:text-white hover:border-light-green transition-all duration-200 ease-in-out">
+            Vector
+          </h3>
+        </Link>
+        <div className="normalMenu hidden top-0 right-0 sm:flex absolute top-0 z-100  items-center h-18">
+          {/* <div className="flex flex-row h-full bg-light-green items-center logoContainer text-dark-green">
+            <Link href="/">
+              <h2 className="lowercase font-grotesk text-[1.7rem] leading-none tracking-[0.3rem] font-black h-full px-4 -translate-y-1">
+                Vector
+              </h2>
+            </Link>
+          </div> */}
 
-        <nav className="flex flex-row h-full bg-harsh-blue gap-5 uppercase tracking-wider text-white border-b-1 border-r-1 items-center px-10">
-          <Link href="/viruses">
-            <div className="hoverHeader">Viruses</div>
-          </Link>
-          <Link href="/about">About</Link>
-          <Link href="/cart" data-test-id="cart-link">
-            Cart:{' '}
-            <span data-test-id="cart-count">
-              {`[${totalCartItems ? totalCartItems : 'empty'}]`}
-            </span>
-          </Link>
-          {totalCartItems > 0 ? <Link href="/checkout">Checkout</Link> : null}
-        </nav>
+          {/* <nav className="flex flex-row h-full bg-harsh-blue gap-5 uppercase tracking-wider text-white border-b-1 border-r-1 items-center px-10"> */}
+          <nav className="flex flex-row h-full gap-5 text-lg uppercase tracking-widest items-center px-10 font-black text-light-green border-b-1 border-light-green/50">
+            <Link href="/viruses">
+              <div className="hoverHeader">Viruses</div>
+            </Link>
+            <Link href="/about">About</Link>
+            <Link href="/cart" data-test-id="cart-link">
+              Cart:{' '}
+              <span data-test-id="cart-count">
+                {`[${totalCartItems ? totalCartItems : 'empty'}]`}
+              </span>
+            </Link>
+            {totalCartItems > 0 ? <Link href="/checkout">Checkout</Link> : null}
+          </nav>
+        </div>
       </div>
       <BurgerMenu totalCartItems={totalCartItems} />
     </header>
