f27d7f26a730e62325917d27e9b430c4f98b3589
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-22 08:17:25 +0300

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
+import BigLogo from './components/BigLogo';
 import Featured from './components/Featured';
 import News from './components/News';
 
 export default function Home() {
   return (
     <>
-      <div className="mb-10 lg:mt-12 sm:mb-14 md:mb-21  lg:mb-27 xl:mb-55 hidden sm:block">
-        <h1 className="font-grotesk font-black lg:justify-self-center text-center lowercase text-[#3acda8] text-[30vw] lg:text-[26vw] leading-none lg:leading-[12vw] -z-1">
+      {/* <div className="sm:mb-10 lg:mt-12 sm:mb-14 md:mb-21 lg:mb-27 xl:mb-55 sm:block sticky top-0">
+        <h1 className="font-grotesk font-black lg:justify-self-center text-right sm:text-center lowercase text-[#3acda8] text-8xl sm:text-[30vw] lg:text-[26vw] leading-none lg:leading-[12vw] -z-1">
           Vector
         </h1>
         <div className="sm:mt-[0vh] lg:mt-[6vh] xl:mt-[9vh] text-right -translate-x-[18.5vw] text-[3.7vw] text-white -z-10 uppercase font-thin font-grotesk">
@@ -21,9 +22,12 @@ export default function Home() {
             className="sm:w-[27vw] h-auto drop-shadow-xl/20"
           />
         </div>
+      </div> */}
+      <BigLogo />
+      <div className="pt-[40vw]">
+        <Featured />
+        <News />
       </div>
-      <Featured />
-      <News />
     </>
   );
 }
