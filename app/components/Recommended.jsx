f73c160bc7b4078eccffd4ebb59bb616cec73b9b
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-29 07:53:03 +0300

    create Recommended component

---
+++ app/components/Recommended.jsx
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { getVirusesInsecure } from '../../database/viruses';
+
+export default async function Recommended() {
+  const viruses = await getVirusesInsecure();
+
+  const shuffleArray = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
+  const recommendedViruses = shuffleArray(viruses).slice(0, 4);
+
+  console.log('recommended', recommendedViruses);
+
+  return (
+    <div className="featured pt-5 pb-10 w-full">
+      <h2 className="special">Recommended Viruses</h2>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-5 sm:gap-10 xl:gap-20 px-5 sm:px-20 md:px-10 lg:px-20 py-5 xl:px-20 md:py-8">
+        {recommendedViruses.map((virus) => {
+          return (
+            <div key={`virusId-${virus.id}`} className="">
+              <Link href={`/viruses/${virus.id}`}>
+                <div className="relative w-full aspect-square flex items-center justify-center group">
+                  <h3 className="z-10 text-white text-3xl text-center sm:text-2xl md:text-3xl lg:text-4xl xl:text-5 xl 2xl:text-6xl font-grotesk text-shadow-lg/50">
+                    {virus.virusName}
+                  </h3>
+                  <Image
+                    src={`/viruses/${virus.image}`}
+                    alt={virus.virusName}
+                    className="object-cover object-center absolute inset-0 sm:opacity-70 sm:group-hover:opacity-100"
+                    fill
+                    sizes="(min-width: 3840px) 100vw, (min-width: 2560px) 850px, (min-width: 1920px) 800px, (min-width: 1280px) 430px, (min-width: 768px) 350px, 100vw"
+                    quality={75}
+                  />
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      <Link href="/viruses">
+        <h2 className="w-fit m-auto bg-light-green text-2xl sm:text-4xl rounded-xl p-5 text-dark-green uppercase outline-6 outline-middle-green tracking-wider bigButtonHover mt-5">
+          Browse <span className="font-bold">all viruses</span>
+        </h2>
+      </Link>
+    </div>
+  );
+}
