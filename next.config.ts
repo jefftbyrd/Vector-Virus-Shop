42ed8cd8d0fd434ddbbf1a4018d88080ec95d956
Jeff T Byrd <jeff.t.byrd@gmail.com>
2025-07-30 10:34:42 +0300

    update next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
     dynamicIO: true,
     typedRoutes: true,
   },
+  images: {
+    formats: ['image/webp', 'image/avif'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+    // Add this to see if it helps
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https:;",
+  },
 };
 
 export default nextConfig;
